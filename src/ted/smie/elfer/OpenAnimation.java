257f6d68655a5c23b2f42483bbeb45ecf8d405a1
TedCheng <zhenghuadi@foxmail.com>
2015-01-04 19:00:29 +0800

    Stop Animation by LongClick

--- src/ted/smie/elfer/OpenAnimation.java
+++ src/ted/smie/elfer/OpenAnimation.java
@@ -13,22 +13,22 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.view.WindowManager;
 import android.widget.RelativeLayout.LayoutParams;
 
 /**
- * Description:
- * <br/>site: <a href="http://www.crazyit.org">crazyit.org</a>
- * <br/>Copyright (C), 2001-2014, Yeeku.H.Lee
- * <br/>This program is protected by copyright laws.
- * <br/>Program Name:
- * <br/>Date:
- * @author  Yeeku.H.Lee kongyeeku@163.com
- * @version  1.0
+ * Description: <br/>
+ * site: <a href="http://www.crazyit.org">crazyit.org</a> <br/>
+ * Copyright (C), 2001-2014, Yeeku.H.Lee <br/>
+ * This program is protected by copyright laws. <br/>
+ * Program Name: <br/>
+ * Date:
+ * 
+ * @author Yeeku.H.Lee kongyeeku@163.com
+ * @version 1.0
  */
-public class OpenAnimation extends Activity
-	implements OnClickListener
-{
+public class OpenAnimation extends Activity {
 	SurfaceView surfaceView;
 	MediaPlayer mPlayer;
 	// 记录当前视频的播放位置
@@ -47,67 +47,72 @@ public class OpenAnimation extends Activity
 		}
 
 	};
+	// detect if the video finishes
 	Runnable exit = new Runnable() {
 		@Override
 		public void run() {
-			if(mPlayer.getCurrentPosition()==mPlayer.getDuration())
-			{
-				Intent intent = new Intent();  
-                intent.setClass(OpenAnimation.this,Login.class);  
-                startActivity(intent);  
-                OpenAnimation.this.finish();
-			}
-			else
+			if (mPlayer.getCurrentPosition() == mPlayer.getDuration()) {
+				Intent intent = new Intent();
+				intent.setClass(OpenAnimation.this, Login.class);
+				startActivity(intent);
+				OpenAnimation.this.finish();
+			} else
 				autoPlayHandler.postDelayed(exit, 2000);
 		}
 	};
+
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.openanim);
-		//屏幕长亮
-				getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); 
+		// 屏幕长亮
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		// 创建MediaPlayer
 		mPlayer = new MediaPlayer();
 		surfaceView = (SurfaceView) this.findViewById(R.id.surfaceView);
-		surfaceView.setOnClickListener(this);
+		surfaceView.setOnLongClickListener(new OnLongClickListener() {
+			
+			@Override
+			public boolean onLongClick(View v) {
+				// TODO Auto-generated method stub
+				autoPlayHandler.removeCallbacks(exit);
+				Intent intent = new Intent();
+				intent.setClass(OpenAnimation.this, Login.class);
+				startActivity(intent);
+				OpenAnimation.this.finish();
+				return false;
+			}
+		});
 		// 设置播放时打开屏幕
 		surfaceView.getHolder().setKeepScreenOn(true);
 		surfaceView.getHolder().addCallback(new SurfaceListener());
 		autoPlayHandler.postDelayed(r, 1000);
 	}
+// Replace By Long Click
+//	@Override
+//	public void onClick(View source) {
+//		try {
+//			switch (source.getId()) {
+//			// 播放按钮被单击
+//			case R.id.surfaceView:
+//				Intent intent = new Intent();
+//				intent.setClass(OpenAnimation.this, Login.class);
+//				startActivity(intent);
+//				OpenAnimation.this.finish();
+//				break;
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 
-	@Override
-	public void onClick(View source)
-	{
-		try
-		{
-			switch (source.getId())
-			{
-				// 播放按钮被单击
-				case R.id.surfaceView:
-					Intent intent = new Intent();  
-	                intent.setClass(OpenAnimation.this,Login.class);  
-	                startActivity(intent);  
-	                OpenAnimation.this.finish();
-					break;
-			}
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	private void play() throws IOException
-	{
+	private void play() throws IOException {
 		mPlayer.reset();
 		mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 		// 设置需要播放的视频
 		mPlayer.setDataSource("/storage/sdcard1/m.mp4");
 		// 把视频画面输出到SurfaceView
-		mPlayer.setDisplay(surfaceView.getHolder());  //①
+		mPlayer.setDisplay(surfaceView.getHolder()); // ①
 		mPlayer.prepare();
 		// 获取窗口管理器
 		WindowManager wManager = getWindowManager();
@@ -115,52 +120,42 @@ public class OpenAnimation extends Activity
 		// 获取屏幕大小
 		wManager.getDefaultDisplay().getMetrics(metrics);
 		// 设置视频保持纵横比缩放到占满整个屏幕
-		surfaceView.setLayoutParams(new LayoutParams(metrics.widthPixels
-			, mPlayer.getVideoHeight() * metrics.widthPixels
-			/ mPlayer.getVideoWidth()));
+		surfaceView.setLayoutParams(new LayoutParams(metrics.widthPixels,
+				mPlayer.getVideoHeight() * metrics.widthPixels
+						/ mPlayer.getVideoWidth()));
 		mPlayer.start();
 	}
 
-	private class SurfaceListener implements SurfaceHolder.Callback
-	{
+	private class SurfaceListener implements SurfaceHolder.Callback {
 		@Override
-		public void surfaceChanged(SurfaceHolder holder, int format,
-			int width, int height)
-		{
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height) {
 		}
 
 		@Override
-		public void surfaceCreated(SurfaceHolder holder)
-		{
-			if (position > 0)
-			{
-				try
-				{
+		public void surfaceCreated(SurfaceHolder holder) {
+			if (position > 0) {
+				try {
 					// 开始播放
 					play();
 					// 并直接从指定位置开始播放
 					mPlayer.seekTo(position);
 					position = 0;
-				}
-				catch (Exception e)
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		}
 
 		@Override
-		public void surfaceDestroyed(SurfaceHolder holder)
-		{
+		public void surfaceDestroyed(SurfaceHolder holder) {
 		}
 	}
 
 	// 当其他Activity被打开，暂停播放
 	@Override
-	protected void onPause()
-	{
-		if (mPlayer.isPlaying())
-		{
+	protected void onPause() {
+		if (mPlayer.isPlaying()) {
 			// 保存当前的播放位置
 			position = mPlayer.getCurrentPosition();
 			mPlayer.stop();
@@ -169,10 +164,10 @@ public class OpenAnimation extends Activity
 	}
 
 	@Override
-	protected void onDestroy()
-	{
+	protected void onDestroy() {
 		// 停止播放
-		if (mPlayer.isPlaying()) mPlayer.stop();
+		if (mPlayer.isPlaying())
+			mPlayer.stop();
 		// 释放资源
 		mPlayer.release();
 		super.onDestroy();
